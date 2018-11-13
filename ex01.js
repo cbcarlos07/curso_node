d08598af5da5f359b4a0ac6ba9539be4b8466ca4
Carlos Bruno <cbcarlos08@gmail.com>
2018-11-13 15:54:35 -0400

    Seção 3 Aula 20 Node - Exercicio 05: This

---
+++ ex01.js
@@ -0,0 +1,4 @@
+
+
+const ola = () => console.log('Ola node!')
+setInterval(ola, 1000)
---
+++ ex03_singleton.js
@@ -0,0 +1,5 @@
+let numero = 1
+function exibirProximo(){
+    console.log(numero++)
+}
+module.exports = { exibirProximo }
